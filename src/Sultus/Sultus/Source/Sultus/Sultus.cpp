ff3d917288e2eb852abfaea73be5383b62316f37
bjlinnertz <bjlinnertz@gmail.com>
2019-09-01 17:41:20 -0500

    Fixed Melee sound effect and added new healing sound effect

--- src/Sultus/Sultus/Source/Sultus/Sultus.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-#include "Sultus.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sultus, "Sultus" );
- 
