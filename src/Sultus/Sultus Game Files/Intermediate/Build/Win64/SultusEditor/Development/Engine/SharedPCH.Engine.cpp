eed80fa11438cd95d0ea1489f42b583bbf423cfe
Ian Jeannin <ian.jeannin@loop.colum.edu>
2019-09-22 14:05:13 -0500

    Opening Unreal in new version
    Lot's of changes caused by this, not sure if it will cause any issues when pushed. Will check in before doing so.

--- src/Sultus/Sultus Game Files/Intermediate/Build/Win64/SultusEditor/Development/Engine/SharedPCH.Engine.cpp
+++ src/Sultus/Sultus Game Files/Intermediate/Build/Win64/SultusEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/major/OneDrive/Documents/Unreal Projects/sultus/src/Sultus/Sultus Game Files/Intermediate/Build/Win64/SultusEditor/Development/Engine/SharedPCH.Engine.h"
+#include "C:/Users/jeann/Documents/GitHub/sultus/src/Sultus/Sultus Game Files/Intermediate/Build/Win64/SultusEditor/Development/Engine/SharedPCH.Engine.h"
